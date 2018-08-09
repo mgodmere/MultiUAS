178e5c4a7743208646d0a94648ceed1d6b45c3cb
mgodmere <matthewgodmere@gmail.com>
2018-08-09 17:49:50 -0600

    Install

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,9 +7,10 @@ sudo apt-get -y install cmake python python-pip
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get -y update
-sudo apt-get -y install ros-kinetic-desktop-full
+sudo apt-get -y install ros-melodic-desktop-full
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 sudo apt-get -y install python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo pip install jinja2 numpy toml
 
 # PX4 SITL
 cd Firmware
